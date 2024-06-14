40cf11fd5fbd67d566d914370304756d54af649e
Zshoham <ShohamZarfti@gmail.com>
2024-06-14 15:26:30 +0300

    added atuin install script

---
+++ config/scripts/install-atuin.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+wget https://github.com/atuinsh/atuin/releases/download/v18.3.0/atuin-x86_64-unknown-linux-gnu.tar.gz -O /tmp/atuin.tar.gz
+tar -xf /tmp/atuin.tar.gz -C /tmp
+ls -la /tmp
+ls -la
+install -m 755 /tmp/atuin-x86_64-unknown-linux-gnu/atuin /usr/bin/atuin
