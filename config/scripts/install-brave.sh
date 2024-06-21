1af195b91ccb8808d7112a686ac8cef98ad311ce
Zshoham <ShohamZarfti@gmail.com>
2024-06-21 11:13:12 +0300

    remove old way of installing brave

--- config/scripts/install-brave.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-curl --output-dir "/etc/yum.repos.d/" -O https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-
-
